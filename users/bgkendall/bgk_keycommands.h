3f7120e1d8e0b78349bcc7cb0e1216674a7ef3dc
B. G. Kendall <69401250+bgkendall@users.noreply.github.com>
2024-06-26 20:18:41 -0400

    Remove unused function header

--- users/bgkendall/bgk_keycommands.h
+++ users/bgkendall/bgk_keycommands.h
@@ -27,12 +27,3 @@ bool bgkey_unregister_command_for_tab(bool immediate);
 bool bgkey_register_forward_command_tab(void);
 
 bool bgkey_register_backward_command_tab(void);
-
-
-// Grave/Escape, but with any keys and modifiers
-//
-bool bgkey_mod_munge(bool pressed,
-                     uint16_t natural_key,
-                     uint16_t modified_key,
-                     uint8_t trigger_modifiers,
-                     bool release_modifiers);
