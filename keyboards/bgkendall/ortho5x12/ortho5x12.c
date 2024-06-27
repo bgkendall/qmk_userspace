6dd057bb999d566dbc118e821f2d0376a622c0d3
B. G. Kendall <69401250+bgkendall@users.noreply.github.com>
2024-06-26 20:16:57 -0400

    Move Caps Word handling of Tap Dances to user area

--- keyboards/bgkendall/ortho5x12/ortho5x12.c
+++ keyboards/bgkendall/ortho5x12/ortho5x12.c
@@ -451,38 +451,6 @@ const key_override_t** key_overrides = (const key_override_t *[])
 #endif // ¬VIAL_ENABLE
 
 
-/*****************************************************************************
- * CAPS WORD                                                                 *
- *****************************************************************************/
-
-#ifdef CAPS_WORD_ENABLE
-
-bool caps_word_press_user(uint16_t keycode)
-{
-    switch (keycode)
-    {
-        // Keycodes that continue Caps Word, with shift applied:
-        case KC_A ... KC_Z:
-        case KC_MINS:
-        case QK_TAP_DANCE: // Only apply to first tap dance key
-            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key
-            return true;
-
-        // Keycodes that continue Caps Word, without shifting:
-        case KC_1 ... KC_0:
-        case KC_BSPC:
-        case KC_DEL:
-        case KC_UNDS:
-            return true;
-
-        default:
-            return false;  // Deactivate Caps Word
-    }
-}
-
-#endif // CAPS_WORD_ENABLE
-
-
 #ifndef KEYBOARD_handwired_bgkendall_ortho5x12_rev2m
 
 void keyboard_post_init_kb(void)
