d5c0158880628a81b626ecc6d28afef160e16314
B. G. Kendall <69401250+bgkendall@users.noreply.github.com>
2024-12-13 00:15:06 -0500

    Add default keymap for Ortho 5×12 when not using Vial

---
+++ keyboards/bgkendall/ortho5x12/keymaps/default_r4/keymap.c
@@ -0,0 +1,43 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+	[0] = LAYOUT(KC_ESC,  KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0,    KC_BSPC,
+                 KC_TAB,  KC_Q, KC_W, KC_D, KC_P, KC_F, KC_J, KC_L, KC_O, KC_Y, KC_SCLN, KC_EQL,
+                 KC_QUOT, KC_A, KC_R, KC_S, KC_T, KC_G, KC_M, KC_N, KC_E, KC_I, KC_U,    KC_MINS,
+                 KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_K, KC_H, KC_COMM, KC_DOT, KC_UP, RSFT_T(KC_SLSH),
+                 KC_LCTL, KC_LALT, MO(1), KC_LGUI, LT(3,KC_SPC), RGUI_T(KC_ENT), TD(0), KC_LEFT, KC_DOWN, KC_RGHT),
+    [1] = LAYOUT(QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
+                 KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                 KC_GRV,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PIPE, KC_TRNS, KC_TILD,
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_PGUP, KC_BSLS,
+                 KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,     KC_TRNS,       KC_PENT,     KC_TRNS,   KC_HOME, KC_PGDN, KC_END),
+    [2 ... (DYNAMIC_KEYMAP_LAYER_COUNT - 1)] = LAYOUT(
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,     KC_TRNS,      KC_TRNS,     KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS
+      )
+};
+
+
+#ifdef COMBO_ENABLE
+
+const uint16_t PROGMEM tabQ[] = {KC_TAB, KC_Q, COMBO_END};
+const uint16_t PROGMEM dotSlash[] = {KC_DOT, KC_SLASH, COMBO_END};
+combo_t key_combos[] =
+{
+    COMBO(tabQ, KC_ESC),
+    COMBO(dotSlash, KC_BSLS)
+};
+
+#endif // COMBO_ENABLE
+
+
+#ifdef ENCODER_MAP_ENABLE
+
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0 ... DYNAMIC_KEYMAP_LAYER_COUNT - 1] = { ENCODER_CCW_CW(KC_LEFT, KC_RGHT) }
+};
+
+#endif // ENCODER_MAP_ENABLE
