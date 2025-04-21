ddbbb22054a8c6024d31ea9e6ae094e90c83c9af
B. G. Kendall <69401250+bgkendall@users.noreply.github.com>
2025-04-21 13:04:08 -0400

    Update Undermountain firmware based on final hardware setup

--- keyboards/bgkendall/undermountain/keymaps/default/keymap.c
+++ keyboards/bgkendall/undermountain/keymaps/default/keymap.c
@@ -5,24 +5,54 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_A,    KC_B,    KC_C,
-        KC_X,    KC_Y,    KC_Z
+        LT(1, KC_WAKE), MO(2),
+        KC_SLEP,        KC_MUTE
     ),
-    [1 ... (DYNAMIC_KEYMAP_LAYER_COUNT - 1)] = LAYOUT(
-        KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS
-      )
-
+    [1] = LAYOUT(
+        KC_TRNS, KC_TRNS,
+        BL_BRTG, KC_TRNS
+    ),
+    [2] = LAYOUT(
+          KC_CAPS, KC_TRNS,
+          MO(3),   MO(4)
+    ),
+    [3] = LAYOUT(
+          KC_NO,   KC_NO,
+          KC_TRNS, QK_BOOT
+    ),
+    [4] = LAYOUT(
+          KC_NO,   KC_NO,
+          QK_BOOT, KC_TRNS
+    ),
+    [5 ... (DYNAMIC_KEYMAP_LAYER_COUNT - 1)] = LAYOUT(
+          KC_TRNS, KC_TRNS,
+          KC_TRNS, KC_TRNS
+    )
 };
 
 #ifdef ENCODER_MAP_ENABLE
 
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0 ... DYNAMIC_KEYMAP_LAYER_COUNT - 1] =
-        {
-            ENCODER_CCW_CW(KC_BRID, KC_BRIU),
-            ENCODER_CCW_CW(KC_VOLD, KC_VOLU)
-        }
+    [0] =
+    {
+        ENCODER_CCW_CW(KC_BRID, KC_BRIU),
+        ENCODER_CCW_CW(KC_VOLD, KC_VOLU)
+    },
+    [1] =
+    {
+        ENCODER_CCW_CW(BL_DOWN, BL_UP  ),
+        ENCODER_CCW_CW(KC_TRNS, KC_TRNS)
+    },
+    [2] =
+    {
+        ENCODER_CCW_CW(KC_NO, KC_NO),
+        ENCODER_CCW_CW(KC_NO, KC_NO)
+    },
+    [3 ... DYNAMIC_KEYMAP_LAYER_COUNT - 1] =
+    {
+        ENCODER_CCW_CW(KC_TRNS, KC_TRNS),
+        ENCODER_CCW_CW(KC_TRNS, KC_TRNS)
+    }
 };
 
 #endif // ENCODER_MAP_ENABLE
--- keyboards/bgkendall/undermountain/undermountain.c
+++ keyboards/bgkendall/undermountain/undermountain.c
@@ -1,1 +1,77 @@
 #include QMK_KEYBOARD_H
+
+
+#define INDICATOR_PIN B2
+
+
+#ifdef INDICATOR_PIN
+
+const uint8_t os_indicator_flash_sequence[] =
+{
+    // Pseudo-Morse code to indicate OS type via single colour LED
+    // Bytes are read right-to-left
+    [OS_UNSURE]         = 0b11110101, // U . . —
+    [OS_LINUX]          = 0b10101101, // L . – . .
+    [OS_WINDOWS]        = 0b01010111, // D – . .
+    [OS_MACOS...OS_IOS] = 0b01110111  // M — —
+};
+
+uint32_t flash_os_indicator(uint32_t trigger_time, void* cb_arg)
+{
+    uint8_t* os_indicator_sequence = (uint8_t*)cb_arg;
+
+    // Set indicator pin based on LSB of current flash sequence state
+    // Bit is flipped since LOW is ON and HIGH is OFF
+    writePin(INDICATOR_PIN, !(*os_indicator_sequence & 1));
+
+    if (*os_indicator_sequence)
+    {
+        *os_indicator_sequence >>= 1;
+        return 333;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+bool process_detected_host_os_kb(os_variant_t detected_os)
+{
+    static uint8_t os_indicator_sequence = 0;
+
+    if (detected_os <= OS_IOS)
+    {
+        os_indicator_sequence = os_indicator_flash_sequence[detected_os];
+        defer_exec(100, flash_os_indicator, (void*)&os_indicator_sequence);
+    }
+
+    return true;
+}
+
+#endif // INDICATOR_PIN
+
+bool process_record_kb(uint16_t keycode, keyrecord_t* record)
+{
+    bool process = true;
+
+    // Work-around for Layer-Tap only supporting basic key codes:
+    if (record->event.pressed &&
+        record->tap.count > 0 &&
+        (keycode & QK_BASIC_MAX) == KC_WAKE)
+    {
+#       ifdef BACKLIGHT_ENABLE
+        backlight_toggle();
+#       endif
+        process = false;
+    }
+
+    return process;
+}
+
+void keyboard_post_init_kb(void)
+{
+#   ifdef INDICATOR_PIN
+    setPinOutput(INDICATOR_PIN); // Enable pin for front indicator light
+    writePinHigh(INDICATOR_PIN); // Turn OFF front indicator light
+#   endif
+}
