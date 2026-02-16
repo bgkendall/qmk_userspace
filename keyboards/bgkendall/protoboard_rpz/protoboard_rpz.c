e7da454ee3d75cb0fffbb8e1f55700ea7292bfdb
B. G. Kendall <69401250+bgkendall@users.noreply.github.com>
2026-02-15 22:53:19 -0500

    Test round-robin matrix with standard QMK
    It works!

--- keyboards/bgkendall/protoboard_rpz/protoboard_rpz.c
+++ keyboards/bgkendall/protoboard_rpz/protoboard_rpz.c
@@ -1,33 +1,7 @@
-#include QMK_KEYBOARD_H
+#include "quantum.h"
 
-
-#ifdef RGBLIGHT_ENABLE
-
-enum RGB_LAYERS
-{
-    RGBL_CAPS = 0
+const matrix_row_t matrix_mask[] = {
+    0b0110,
+    0b0101,
+    0b0011
 };
-
-const rgblight_segment_t PROGMEM rgb_pink_layer[] =
-        RGBLIGHT_LAYER_SEGMENTS( {0, RGBLED_NUM, 0, 224, 255} );
-
-const rgblight_segment_t* const PROGMEM rgb_layers[] = RGBLIGHT_LAYERS_LIST
-(
-    [RGBL_CAPS] = rgb_pink_layer
-);
-
-bool led_update_kb(led_t led_state)
-{
-    rgblight_set_layer_state(RGBL_CAPS, led_state.caps_lock);
-    return true;
-}
-
-#endif // RGBLIGHT_ENABLE
-
-
-void keyboard_post_init_kb(void)
-{
-#ifdef RGBLIGHT_ENABLE
-    rgblight_layers = rgb_layers;
-#endif // RGBLIGHT_ENABLE
-}
