fe5eb3913a31b380c0810767e3152d983c0bf1a6
B. G. Kendall <69401250+bgkendall@users.noreply.github.com>
2026-03-23 12:44:45 -0400

    Copy Vault 35 keymap to userspace

---
+++ keyboards/bgkendall/split_test/keymaps/left/keymap.c
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [0] = LAYOUT(
+        KC_Q, KC_W, KC_D, KC_P, KC_F, KC_J, KC_L, KC_O
+    )
+};
---
+++ keyboards/bgkendall/split_test/keymaps/right/config.h
@@ -0,0 +1,7 @@
+// Copyright 2025 bgkendall (@bgkendall)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define SERIAL_USART_TX_PIN GP8
+#define SERIAL_USART_RX_PIN GP9
