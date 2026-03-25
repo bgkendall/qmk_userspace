232c4d9ee3c0f27b4cb95f6321cc1ae658d74495
B. G. Kendall <69401250+bgkendall@users.noreply.github.com>
2026-03-25 13:07:37 -0400

    Add Split Test keyboard common configuration
    Keymaps accidentally added by fe5eb3913a31b380c0810767e3152d983c0bf1a6

---
+++ keyboards/bgkendall/split_test/config.h
@@ -0,0 +1,8 @@
+// Copyright 2025 bgkendall (@bgkendall)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+
+#define SERIAL_USART_FULL_DUPLEX
